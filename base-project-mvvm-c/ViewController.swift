318cfba0458aaec93fc72cb1f6a1f46ea08283e9
rahmanarif47 <rahmanarif.9764@gmail.com>
2021-12-06 13:59:15 +0700

    Initial Commit

---
+++ base-project-mvvm-c/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  base-project-mvvm-c
+//
+//  Created by Arif Rahman Sidik on 06/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ base-project-mvvm-cTests/base_project_mvvm_cTests.swift
@@ -0,0 +1,33 @@
+//
+//  base_project_mvvm_cTests.swift
+//  base-project-mvvm-cTests
+//
+//  Created by Arif Rahman Sidik on 06/12/21.
+//
+
+import XCTest
+@testable import base_project_mvvm_c
+
+class base_project_mvvm_cTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
